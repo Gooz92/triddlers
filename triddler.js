40df4ba86880f7ae9b0c089d9dca1344b2172d80
Anton Kurhuzenkau <Anton_Kurhuzenkau@epam.com>
2020-03-03 12:43:31 +0300

    development

--- triddler.js
+++ triddler.js
@@ -25,6 +25,10 @@ const path = [
   [
     a => - a / 2,
     a => sin60 * a
+  ],
+  [
+    a => -a,
+    a => 0,
   ]
 ]
 
@@ -32,13 +36,15 @@ function calculateRestSides(sides) {
   const [ a, b, c, d ] = sides;
   return [
     ...sides,
-    a + b - d
+    a + b - d,
+    d
   ];
 }
 
 function drawBorders(ctx, x0, y0, cellSize, sides) {
 
-  const sidesPx = calculateRestSides(sides).map(s => s * cellSize);
+  const allSides = calculateRestSides(sides);
+  const sidesPx = allSides.map(s => s * cellSize);
 
   ctx.moveTo(x0, y0);
 
@@ -51,7 +57,6 @@ function drawBorders(ctx, x0, y0, cellSize, sides) {
 
     x += nextDx(side);
     y += nextDy(side);
-
     ctx.lineTo(x, y);
 
     vpx.push([ x, y ]);
@@ -60,6 +65,19 @@ function drawBorders(ctx, x0, y0, cellSize, sides) {
   ctx.closePath();
 
   ctx.stroke();
+
+  const gridEnds = calculateGridEnds(vpx, cellSize, allSides);
+
+  gridEnds.forEach(([ xe, ye ]) => {
+    ctx.beginPath();
+    ctx.arc(xe, ye, 2, 0, Math.PI * 2, false);
+    ctx.closePath();
+    ctx.stroke();
+  });
+}
+
+function drawGrid(sides, gridEnds) {
+  
 }
 
 function calculateGridEnds(vertexes, cellSize, sides) {
@@ -68,11 +86,16 @@ function calculateGridEnds(vertexes, cellSize, sides) {
 
   sides.forEach((side, index) => {
     const v = vertexes[index];
-    const 
+    let [ x, y ] = v;
     for (let i = 0; i < side; i++) {
-      
+      x += path[index][0](cellSize);
+      y += path[index][1](cellSize);
+
+      gridEnds.push([ x, y ]);
     }
   });
+
+  return gridEnds;
 }
 
-drawBorders(ctx, 250, 250, 40, [ 2, 3, 2, 3 ]);
+drawBorders(ctx, 250, 250, 40, [ 1, 4, 1, 4 ]);
